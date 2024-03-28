4b9e18c1aa586d2444ea8edba24a326f97534d79
Nicholas Murray <nmurr13@protonmail.com>
2024-03-28 01:07:29 -0700

    Commit: NFL queries 3

--- nfl.sql
+++ nfl.sql
@@ -28,16 +28,20 @@ SELECT * FROM players ORDER BY salary DESC LIMIT 50;
  SELECT AVG(salary) FROM players;
 
 -- 8. The names and positions of players with a salary above 10_000_000
+
 SELECT name, position FROM players WHERE salary >10000000;
 
 -- 9. The player with the highest salary in the NFL
 
+SELECT name from players ORDER BY salary DESC LIMIT 1;
 
 -- 10. The name and position of the first 100 players with the lowest salaries
 
+SELECT name, position FROM players ORDER BY salary ASC LIMIT 100;
 
 -- 11. The average salary for a DE in the nfl
 
+ SELECT AVG(salary) FROM players WHERE position ='DE';
 
 -- 12. The names of all the players on the Buffalo Bills
 
